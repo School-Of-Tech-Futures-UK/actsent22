cdf7228f3fc87ae3cf78a7ea1936a3023a37d4b9
khaashif1 <khaashif.riaz@accenture.com>
2022-03-07 12:01:15 +0000

    api testing

--- back-end/server.test.js
+++ back-end/server.test.js
@@ -6,22 +6,16 @@ const request = require('supertest');
 describe('GET /venue', function() {
   it('responds with list of venues', async function() {
     const response = await request(app)
-      .get('/venue')
+      .get('/api/events')
     console.log(response.headers)
     expect(response.headers["content-type"]).toMatch(/json/)
-    expect(response.body).toEqual([
-      {
-        venue_name: 'Arena 1',
-        location: 'London',
-        image: 'https://picsum.photos/id/101/200/200',
-        description: 'The first arena'
-      },
-      {
-        venue_name: 'Bills Bar',
-        location: 'Manchester',
-        image: 'https://picsum.photos/id/1047/200/200',
-        description: 'A bar belonging to Bill'
-      }
-    ])
+    expect(Object.keys(response.body[0])).
+    toBe(['venue_id', 'event_id', 'date', 
+    'event_name', 'event_description', 'event_image', 
+    'artist_name', 'artist_email', 'genre', 'status', 
+    'venue_name', 'venue_geolocation', 'venue_address'])
+    
   })
+
+
 })
