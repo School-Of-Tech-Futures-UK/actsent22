7d095c5cf6053aefc9802049cbb4b18c9f31e6a2
Mariola Rubio <mariola.rubio.canton@accenture.com>
2022-03-08 12:05:21 +0000

    Added logging for API calls.

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 const express = require('express')
 const cors = require('cors')
+const winston = require('winston')
 const postgres = require('pg-promise')()
 const app = express()
 
@@ -11,10 +12,18 @@ const db = postgres({
   password: process.env.DB_PASSWORD || 'gigstr'
 })
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({ timestamp: true })
+  ]
+})
+
 app.use(cors())
 app.use(express.json())
 
-app.post('/api/create_event', (req, res) => {
+app.post('/api/create_event', async (req, res) => {
   const venueId = req.body.id
   const eventName = req.body.eventName
   const eventDescription = req.body.eventDescription
@@ -24,13 +33,42 @@ app.post('/api/create_event', (req, res) => {
   const genre = req.body.genre
   const status = req.body.status
   const date = req.body.date
-  console.log('body:', req.body)
-  console.log('atts:', [venueId, date, eventName, eventDescription, eventImage, artistName, artistEmail, genre, status])
-  console.log('db==', db)
-  db.query(
-    'INSERT INTO event(venue_id, date, event_name, event_description, event_image, artist_name, artist_email, genre, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8 ,$9) RETURNING event_id;',
-    [venueId, date, eventName, eventDescription, eventImage, artistName, artistEmail, genre, status])
-    .then(res.status(200).send('Event creation successful'))
+
+  logger.log({
+    level: 'info',
+    message: 'API request received to create event',
+    venueId,
+    eventName,
+    eventDescription,
+    eventImage,
+    artistName,
+    artistEmail,
+    genre,
+    status,
+    date
+  })
+
+  try {
+    const newEvent = await db.query(
+      'INSERT INTO event(venue_id, date, event_name, event_description, event_image, artist_name, artist_email, genre, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8 ,$9) RETURNING event_id;',
+      [venueId, date, eventName, eventDescription, eventImage, artistName, artistEmail, genre, status])
+    res.status(200).send('Event creation successful')
+    logger.log({
+      level: 'info',
+      message: 'Event created successfully',
+      eventId: newEvent.event_id,
+      eventName,
+      eventsCreated: 1
+    })
+  } catch (error) {
+    logger.log({
+      level: 'error',
+      message: 'Event creation unsuccessful',
+      error: error.toString(),
+      eventName
+    })
+    res.status(500).send('Event creation unsuccessful: ' + error.toString)
+  }
 })
 
 // get all events
@@ -41,12 +79,38 @@ app.get('/api/events', async (req, res) => {
 })
 
 // update status of existing event in event table
-app.put('/api/event/status', (req, res) => {
+app.put('/api/event/status', async (req, res) => {
   const new_status = req.body.status
   const event_id = req.body.event_id
-  console.log('status', new_status)
-  db.query(`UPDATE event SET status = '${new_status}' WHERE event_id = ${event_id}; `)
-    .then(res.status(200).send('Status updated successfully'))
+
+  logger.log({
+    level: 'info',
+    message: 'API request received to update event status',
+    new_status,
+    event_id
+  })
+
+  try {
+    await db.query(`UPDATE event SET status = '${new_status}' WHERE event_id = ${event_id}; `)
+    res.status(200).send('Status updated successfully')
+    logger.log({
+      level: 'info',
+      message: 'Event status updated successfully',
+      new_status,
+      event_id,
+      eventsConfirmed: new_status === 'confirmed' ? 1 : 0,
+      eventsDenied: new_status === 'denied' ? 1 : 0
+    })
+  } catch (error) {
+    logger.log({
+      level: 'error',
+      message: 'Event status update unsuccessful',
+      error: error.toString(),
+      event_id,
+      new_status
+    })
+    res.status(500).send('Event status update unsuccessful: ' + error.toString)
+  }
 })
 
 module.exports = app
